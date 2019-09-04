3f17e0b7667c73d7f34559e0c9e3958b8d951a63
Oleg Birjukov <oleg.birjukov@interstruct.com>
2019-09-05 00:03:08 +0300

    add dynamodb integration

---
+++ src/api/dynamodb.ts
@@ -0,0 +1,12 @@
+import {DynamoDB} from 'aws-sdk';
+
+let options = {};
+
+if (process.env.IS_OFFLINE) {
+  options = {
+    region: 'localhost',
+    endpoint: 'http://localhost:8000',
+  };
+}
+
+export default new DynamoDB.DocumentClient(options);
--- src/functions/hello.ts
+++ src/functions/hello.ts
@@ -1,11 +1,25 @@
 import { APIGatewayProxyHandler } from 'aws-lambda';
 import 'source-map-support/register';
+import dynamodb from '~src/api/dynamodb';
 
 export const handler: APIGatewayProxyHandler = async (event, context) => {
-  return {
-    statusCode: 200,
-    body: JSON.stringify({
-      message: 'Go Serverless Webpack (Typescript) v1.0! Your function executed successfully!'
-    })
-  };
+  const params = {
+    TableName: process.env.DYNAMODB_TABLE_DEVICE_PHONE,
+  } as {TableName: string};
+
+  try {
+    const result = await dynamodb.scan(params).promise();
+
+    return {
+      statusCode: 200,
+      body: JSON.stringify(result.Items),
+    }
+  } catch (err) {
+    console.log(err, process.env.DYNAMODB_TABLE_DEVICE_PHONE);
+    return {
+      statusCode: err.statusCode || 501,
+      headers: { 'Content-Type': 'text/plain' },
+      body: 'Couldn\'t fetch the todo item.',
+    }
+  }
 };
